cdfdbc1fcf2a255b55879360f1e97261136b4c56
Nikita Shpanko <83448047+NikitaShpanko@users.noreply.github.com>
2021-06-16 01:29:59 +0300

    why doesn't it work automatically?

--- js/highlight-current.js
+++ js/highlight-current.js
@@ -1,15 +1,15 @@
-(() => {
-  const highlightLink = function (className) {
-    const url = document.URL;
-    const elems = document.querySelectorAll('a.' + className);
-    console.log(elems);
-    for (i = 0; i < elems.length; i++)
-      if (elems[i].href == url) {
-        elems[i].classList.add(className + '_current');
-        //return; //may have many
-      }
-  };
+// (() => {
+const highlightLink = function (className) {
+  const url = document.URL;
+  const elems = document.querySelectorAll('a.' + className);
+  console.log(elems);
+  for (i = 0; i < elems.length; i++)
+    if (elems[i].href == url) {
+      elems[i].classList.add(className + '_current');
+      //return; //may have many
+    }
+};
 
-  highlightLink('nav__link');
-  highlightLink('mob-nav__link');
-})();
+highlightLink('nav__link');
+highlightLink('mob-nav__link');
+// })();
